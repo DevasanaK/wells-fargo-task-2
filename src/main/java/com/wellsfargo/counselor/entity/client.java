d7a9b83175dd65b7c6a9eaff72f43033b39b00f1
DEVASANA K <118067825+DevasanaK@users.noreply.github.com>
2025-01-22 06:11:58 +0530

    Create client.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/client.java
@@ -0,0 +1,69 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue
+    private long clientId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id", nullable = false)
+    private Advisor advisor;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    private List<Portfolio> portfolios;
+
+    protected Client() {}
+
+    public Client(String name, String email, Advisor advisor) {
+        this.name = name;
+        this.email = email;
+        this.advisor = advisor;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(Advisor advisor) {
+        this.advisor = advisor;
+    }
+
+    public List<Portfolio> getPortfolios() {
+        return portfolios;
+    }
+
+    public void setPortfolios(List<Portfolio> portfolios) {
+        this.portfolios = portfolios;
+    }
+}
